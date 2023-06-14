d4907264ac07f76fd1f8cfb94c146cb14fce734b
Егор Васин <egor.vasin1011@gmail.com>
2023-06-14 21:08:21 +0300

    5

--- src/test2.java
+++ src/test2.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class test2 {
     public static void main(String[] args) {
-
+        System.out.println("Введите время :");
+        Scanner scanner = new Scanner(System.in);
+        int time = scanner.nextInt();
+        if( time <= 6 ){
+            System.out.println("Доброй ночи!");
+        }
+        if( time <= 15 ){
+            System.out.println("Доброй день!");
+        }
+        if( time <= 21 ){
+            System.out.println("Доброй вечер!");
+        }
+        if( time <= 24 ){
+            System.out.println("Доброй ночи!");
+        }
     }
 }
